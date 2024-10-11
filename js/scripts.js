0eec2b6094662dba5f4567b8140b957d01c11614
Jess Amador <amadorjessica98@gmail.com>
2024-10-10 22:39:10 -0400

    small screen menu toggle
    added a navigation bar to small screens. included jquery to open and close item

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,9 @@
-console.log("Hello World!")
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    x.className += ' responsive';
+    if (x.className === 'navtoggle') {
+        x.className += ' responsive';
+      } else {
+        x.className = 'navtoggle';
+      }
+}
